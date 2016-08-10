8b63138f55d83baf27a6872c6099f2ca7f9a8622
iulian vasile <vasileaiulian@gmail.com>
2016-08-10 17:47:26 +0300

    added second client to websocket

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const app = require('express')();
 const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.send("<h1>Here</h1>");
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+
+io.on('connection', function(client){
+  console.log("a client has connected___");
+
+  client.on('disconnect', function(){
+    console.log("a client has ___disconnected");
+  });
+
+  client.on('join', function(data){
+    console.log(data);
+  });
+
+  client.on('messages', function(data){
+    client.broadcast.emit('broad', data);
+  });
+
 });
 
 http.listen(3000, function(){
